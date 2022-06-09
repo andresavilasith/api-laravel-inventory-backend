491595050d5212b9fceade928af01a90a41461ee
Andres Avila <andrs3662@gmail.com>
2022-06-09 16:46:25 -0500

    Rename Test Inventory files

--- tests/Feature/Inventory/CategoryProductControllerTest.php
+++ tests/Feature/Inventory/CategoryProductControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TaxController extends TestCase
+class CategoryProductControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
--- tests/Feature/Inventory/DocumentControllerTest.php
+++ tests/Feature/Inventory/DocumentControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class DocumentController extends TestCase
+class DocumentControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
